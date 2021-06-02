d41f0b870c660127bb50202c40a0a59707fa4e0e
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-06-03 01:19:27 +1000

    created variables, created createGrid() and move()

---
+++ index.js
@@ -0,0 +1,28 @@
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const score = document.getElementById('score')
+let squares = []
+let currentSnake = [2,1,0]
+let direction = 1
+
+function createGrid() {
+    for (var i = 0; i < 100; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        grid.appendChild(square) 
+        squares.push(square)
+    }
+}
+
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake')) 
+
+function move() {
+    const tail = currentSnake.pop() 
+    squares[tail].classList.remove('snake')
+    currentSnake.unshift(currentSnake[0] + direction)
+    squares[currentSnake[0]].classList.add('snake')
+}
+
+move()
