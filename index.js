1db0b7c4ed61887c75aca6653a7b6c7406c6ccda
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-06-03 23:46:30 +1000

    setInterval() and e.keyCode

--- index.js
+++ index.js
@@ -13,16 +13,28 @@ function createGrid() {
         squares.push(square)
     }
 }
-
 createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake')) 
 
-function move() {
+function move() {  
     const tail = currentSnake.pop() 
     squares[tail].classList.remove('snake')
     currentSnake.unshift(currentSnake[0] + direction)
     squares[currentSnake[0]].classList.add('snake')
 }
-
 move()
+
+let timerId = setInterval(move, 1000)
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log("right")
+    } else if (e.keyCode === 38) {
+        console.log("up") 
+    } else if (e.keyCode === 37) {
+        console.log("left")
+    } else if (e.keyCode === 40) {
+        console.log("down")
+    }
+}
